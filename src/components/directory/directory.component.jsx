28c3432b7802fd5c68efc602db3fb1826ef7fb98
paolo <paolo.palmonari@monrif.net>
2021-01-17 09:16:51 +0100

    update menu-items to  navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -55,8 +55,10 @@ class Directory extends React.Component {
             <div className="directory-menu">
                 {
                     this.state.sections.map(
-                        ({title, imageUrl, id, size}) => (
-                            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                        //({title, imageUrl, id, size, linkUrl}) => (
+                            //<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} history={this.props.history} linkUrl={linkUrl} match={this.props.match}/>
+                        ({id, ...otherSectionProps}) => (
+                            <MenuItem key={id} {...otherSectionProps} />
                         )
                     )
                 }
