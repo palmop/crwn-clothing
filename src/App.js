52ac5cfdaceccbb9ab6f0cea7836059989ae243c
paolo <paolo.palmonari@monrif.net>
2021-01-17 08:33:32 +0100

    Route component - introduction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
+const TopicList = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Topic List Page</h1>
+      <Link to={`${props.match.url}/12`}>Topic 12</Link>
+      <Link to={`${props.match.url}/13`}>Topic 13</Link>
+      <Link to={`${props.match.url}/14`}>Topic 14</Link>
+    </div>
+  );
+};
+
+const TopicDetail = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Link to="/topics">Topics</Link>
+      <h1>Topic Detail Page : {props.match.params.topicId} </h1>
+    </div>
+  );
+};
+
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/topics' component={TopicList} />
+        <Route path='/topics/:topicId' component={TopicDetail} />
+        <Route path='/blog/topics/:topicId' component={TopicDetail} />
+      </Switch>
     </div>
   );
 }
